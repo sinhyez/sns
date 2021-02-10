f31a34f189fda7e663deb4fa82f35a3ead0a169b
yoonsung <upthe15752@gmail.com>
2021-02-11 02:52:13 +0900

    수정

--- src/main/java/com/ipro/sns/config/WebConfig.java
+++ src/main/java/com/ipro/sns/config/WebConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    protected static final String loImgPath = "/Users/yoon sung/Desktop/upload/";
+//    protected static final String loImgPath = "/Users/yoon sung/Desktop/upload/";
     protected static final String ubImgPath = "/home/ubuntu/apps/upload/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/upload/**")
-                .addResourceLocations("file://" + loImgPath)
+                .addResourceLocations("file://" + ubImgPath)
                 .setCachePeriod(20);
     }
 }
